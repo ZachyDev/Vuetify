da41d30dcef973f9b57796eff85420738e69bddd
ZachyDev <mosetizachary001@gmail.com>
2020-10-27 17:01:39 +0300

    Navigational Drawers

--- ui-dash/src/components/Navbar.vue
+++ ui-dash/src/components/Navbar.vue
@@ -1,22 +1,55 @@
 <template>
   <nav>
-      <v-toolbar  app>
+      <v-toolbar 
+      dark 
+      height="100" 
+      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+    
+        <!-- toolbar icon -->
+         <v-app-bar-nav-icon @click="toogleSideNav"></v-app-bar-nav-icon>
+
+          <!-- toolbar title -->
           <v-toolbar-title class="text-uppercase grey--text">
               <span class="font-weight-light">Record.</span>
               <span>me!</span>
           </v-toolbar-title>
+          <!-- toolbar title end -->
+
          <v-spacer></v-spacer>
+         
+         <!-- Toolbar btn -->
           <v-btn color="grey">
               <span>Sign Out</span>
-              <v-icon right>mdi-logout-variant</v-icon>
+              <v-icon right>mdi-export</v-icon>
           </v-btn>
+          <!-- Toolbar btn end -->
+
       </v-toolbar>
+      <!-- v-toolbar end -->
+
+    <!-- navigational drawer -->
+    <v-navigation-drawer
+        v-model="drawer" 
+        app 
+        color="deep-purple accent-4">
+        Test
+    </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
 export default {
-    name: "Navbar"
+    name: "Navbar",
+    data() {
+        return {
+            drawer: false
+        }
+    },
+    methods: {
+        toogleSideNav() {
+            return this.drawer = !this.drawer;
+        }
+    }
 }
 </script>
 
