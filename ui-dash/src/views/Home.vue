cca3a6fe7c62a7d96981a4f38ed120100705b241
ZachyDev <mosetizachary001@gmail.com>
2020-10-27 15:27:35 +0300

    Vuetify components

--- ui-dash/src/views/Home.vue
+++ ui-dash/src/views/Home.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <v-btn class="success white--text">
+      <v-icon left>mdi-github</v-icon>
+      <span>Github</span>
+    </v-btn>
+
+    <v-btn class="success white--text">
+      <span>Github</span>
+      <v-icon right large>mdi-twitter</v-icon>
+    </v-btn>
+
+    <v-btn fab small dark color="purple" white>
+      <v-icon >mdi-twitter</v-icon>
+    </v-btn>
+
   </div>
 </template>
 
